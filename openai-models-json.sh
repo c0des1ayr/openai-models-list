4156b3aa80824bf3f424c2abe87175d047b0310b
c0des1ayr <rplatinumedition+git@gmail.com>
2025-07-13 11:33:44 -0500

    Remove wc usage
    Unnecessary, since `head` can accept negative values.

--- openai-models-json.sh
+++ openai-models-json.sh
@@ -5,12 +5,8 @@
 # Get all current models available
 MODELS=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $API_KEY" https://api.openai.com/v1/models | jq "." | grep id | sed 's/"id": //g' | sort)
 
-# Count number of characters (minus one)
-CHAR_COUNT=$(printf "$MODELS" | wc -m)
-FINAL_COUNT=$(( $CHAR_COUNT - 1))
-
 # Fix for JSON
-MODELS_2=$(printf "$MODELS" | head -c$FINAL_COUNT)
+MODELS_2=$(printf "$MODELS" | head -c-1)
 
 # At last, print the JSON for the runner to pipe
 printf "{\n \"models\": [\n$MODELS_2\n ]\n}" | jq "."
